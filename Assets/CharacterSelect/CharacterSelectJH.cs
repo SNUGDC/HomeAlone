1ede29641c00b3cd6e23bcd82c80e19fd3a759f3
EQwon <bluetwintail@naver.com>
2016-01-10 13:50:53 +0900

    Add daily plan scene

---
+++ Assets/CharacterSelect/CharacterSelectJH.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelectJH : MonoBehaviour
+{
+	public GameObject JHInfo;
+
+	public void ChangeRendererState()
+	{
+		if (JHInfo.GetComponent<SpriteRenderer> ().enabled == false)
+		{
+			JHInfo.GetComponent<SpriteRenderer> ().enabled = true;
+			Debug.Log ("True");
+		}
+	}
+}
--- Assets/Scripts/OutButton.cs
+++ Assets/Scripts/OutButton.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class OutButton : MonoBehaviour
 {
 	public GameObject JHInfo;
+    public GameObject CTInfo;
+    public GameObject EQInfo;
+    public GameObject SMInfo;
+    public GameObject OutInfo;
 
 	public void Out ()
 	{
-		if(JHInfo.GetComponent<SpriteRenderer> ().enabled==true)
-		{
-			JHInfo.GetComponent<SpriteRenderer> ().enabled = false;
-			Debug.Log ("False");
-		}
+		JHInfo.GetComponent<SpriteRenderer> ().enabled = false;
+        CTInfo.GetComponent<SpriteRenderer>().enabled = false;
+        EQInfo.GetComponent<SpriteRenderer>().enabled = false;
+        SMInfo.GetComponent<SpriteRenderer>().enabled = false;
+        OutInfo.GetComponent<SpriteRenderer>().enabled = false;
+        Debug.Log ("All False");
 	}
 }
