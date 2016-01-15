d8de0ba41091ef87f6bc6c9332321f5585c5f4a9
jayin301 <alswjdghk@snu.ac.kr>
2016-01-15 12:16:06 +0900

    imply json data, and parse json file in unity.

---
+++ Assets/Data/Character.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using System.IO;
+
+public class Character : MonoBehaviour
+{
+    private string _jsondata;
+    private JsonData _characterdata;
+
+    public void CallCharacterName()
+    {
+
+    }
+
+    void Start()
+    {
+        _jsondata = File.ReadAllText(Application.dataPath + "/Data/Character.json");
+        _characterdata = JsonMapper.ToObject(_jsondata);
+    }
+
+}
