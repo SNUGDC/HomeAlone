efc158b7f120ede9aae5683822517acc587bca0a
EQwon <bluetwintail@naver.com>
2016-01-19 21:50:29 +0900

    Get Delta System Time

---
+++ Assets/TimeTestScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TimeTestScript : MonoBehaviour
+{
+    DateTime StartSysTime;
+    DateTime SysTime;
+
+    void Start()
+    {
+        StartSysTime = System.DateTime.Now;
+    }
+
+    void Update()
+    {
+        SysTime = System.DateTime.Now;
+        Debug.Log(SysTime);
+        Debug.Log(SysTime - StartSysTime);
+    }
+    
+}
