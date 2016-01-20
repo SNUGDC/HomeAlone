1828bc0a9e4e4ad4fd5b2eeec45cff3edcd9fe10
EQwon <bluetwintail@naver.com>
2016-01-21 03:10:07 +0900

    Finish Test Time

--- Assets/TimeTestScript.cs
+++ Assets/TimeTestScript.cs
@@ -1,22 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 using System;
 
 public class TimeTestScript : MonoBehaviour
 {
-    DateTime StartSysTime;
-    DateTime SysTime;
+    public Slider slider;
 
-    void Start()
-    {
-        StartSysTime = System.DateTime.Now;
-    }
+    DateTime SysTime;
+    DateTime UpdatedTime;
+    TimeSpan Delta = new TimeSpan(0,0,30);
 
     void Update()
     {
         SysTime = System.DateTime.Now;
-        Debug.Log(SysTime);
-        Debug.Log(SysTime - StartSysTime);
+        IncreaseValue();
+        TimeOver();
     }
-    
+
+    void IncreaseValue()
+    {
+        if (slider.value != 10 && TimeOver())
+        {
+            slider.value = slider.value + 1;
+            UpdatedTime = SysTime;
+        }
+    }
+
+    bool TimeOver()
+    {
+        if (SysTime - UpdatedTime >= Delta)
+        {
+            return true;
+        }
+        else return false;
+    }
 }
