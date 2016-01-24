15e82db9257a8f288e46058255d559a42676cffe
EQwon <bluetwintail@naver.com>
2016-01-25 02:14:21 +0900

    Make Cheat Key

---
+++ Assets/Main/RemainCostController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemainCostController : MonoBehaviour
+{
+    public int RemainCost;
+
+    void Update()
+    {
+        if(PlayerPrefs.HasKey("RemainCost"))
+            RemainCost = PlayerPrefs.GetInt("RemainCost");
+
+        Save();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("눌리긴하냐");
+            RemainCost = RemainCost + 1;
+            Save();
+        }    
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt("RemainCost", RemainCost);
+    }
+}
