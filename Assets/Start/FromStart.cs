d4c0ea72a9cae31be0db7d1ae6d458acc6e54f26
EQwon <bluetwintail@naver.com>
2016-01-25 01:11:54 +0900

    Make FromStart cod

---
+++ Assets/Start/FromStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FromStart : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        PlayerPrefs.DeleteAll();
+
+        SceneManager.LoadScene("Main");
+    }
+}
