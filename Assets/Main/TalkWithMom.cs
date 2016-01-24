bb6b7ed8d6d6c064aa5e578a070985553b3f37c1
EQwon <bluetwintail@naver.com>
2016-01-24 18:14:50 +0900

    Try to Correct Error But.....

--- Assets/Main/TalkWithMom.cs
+++ Assets/Main/TalkWithMom.cs
@@ -7,9 +7,9 @@ public class TalkWithMom : MonoBehaviour
     public GameObject MomImage;
     public GameObject MomPanel;
 
-    private bool ReadyToChangeText;
-    private bool WillMomTalk = true;
-    private int CurrentLine = 0;
+    public bool ReadyToChangeText;
+    public int WillMomTalk;
+    public int CurrentLine = 0;
 
     string[] talkline = { "우리 딸, 방에는 도착했니? 우리 딸은 알아서 잘 하겠지만 그래도 걱정되네.",
                           "옆에서 챙겨줄 사람이 없으니까 일정관리를 잘해야해. 그렇다고 너무 공부만 하진 말구",
@@ -22,24 +22,22 @@ public class TalkWithMom : MonoBehaviour
 
     void Start()
     {
-        if (WillMomTalk)
-        {
-            MomImage.GetComponent<SpriteRenderer>().enabled = true;
-            MomPanel.GetComponent<SpriteRenderer>().enabled = true;
-            MomPanel.GetComponent<BoxCollider2D>().enabled = true;
-        }
+        WillMomTalk = PlayerPrefs.GetInt("WillMomTalk");
+        WillMomTalk = 1;
     }
 
     void Update()
     {
+        if (WillMomTalk == 1)
+        {
+            TalkAble(true);
+        }
         ReadyToChangeText = MomPanel.GetComponent<ReadyToChangeText>().IsOKToChangeText;
 
         if (CurrentLine > 4)
         {
-            WillMomTalk = false;
-            MomImage.GetComponent<SpriteRenderer>().enabled = false;
-            MomPanel.GetComponent<SpriteRenderer>().enabled = false;
-            MomPanel.GetComponent<BoxCollider2D>().enabled = false;
+            WillMomTalk = 0;
+            TalkAble(false);
         }
 
         if (ReadyToChangeText)
@@ -53,5 +51,17 @@ public class TalkWithMom : MonoBehaviour
         value.text = talkline[CurrentLine];
         MomPanel.GetComponent<ReadyToChangeText>().IsOKToChangeText = false;
         this.CurrentLine++;
+    }
+
+    void TalkAble(bool a)
+    {
+            MomImage.GetComponent<SpriteRenderer>().enabled = a;
+            MomPanel.GetComponent<SpriteRenderer>().enabled = a;
+            MomPanel.GetComponent<BoxCollider2D>().enabled = a;
+    }
+
+    void save()
+    {
+        PlayerPrefs.SetInt("WillMomTalk", WillMomTalk);
     }
 }
