15a9e45a4068c73d08f227130659f62e3e3db3fb
EQwon <bluetwintail@naver.com>
2016-01-27 03:42:05 +0900

    Plus ErrorChecker

--- Assets/Main/Time Table/HandlingImage.cs
+++ Assets/Main/Time Table/HandlingImage.cs
@@ -4,11 +4,23 @@ using System.Collections;
 public class HandlingImage : MonoBehaviour
 {
     public GameObject[] ValueShower;
-    public int Left = 0, Right;
+    public int Right;
+
+    private int Left = 0;
 
     void Update()
     {
+        ErrorChecker();
+
         transform.position = new Vector3((ValueShower[Left].GetComponent<Transform>().position.x + ValueShower[Right].GetComponent<Transform>().position.x) / 2, 0, 0);
         transform.localScale = new Vector3((float)7.5*(Right - Left), 1, 1);
+    }
+
+    void ErrorChecker()
+    {
+        if (Right > 16)
+        {
+            Right = 16;
+        }
     }
 }
