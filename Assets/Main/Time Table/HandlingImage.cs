d79761c48a79be72984a0c97b44582859ed900b2
EQwon <bluetwintail@naver.com>
2016-01-27 03:28:15 +0900

    Make Own_Slider!!

---
+++ Assets/Main/Time Table/HandlingImage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandlingImage : MonoBehaviour
+{
+    public GameObject[] ValueShower;
+    public int Left = 0, Right;
+
+    void Update()
+    {
+        transform.position = new Vector3((ValueShower[Left].GetComponent<Transform>().position.x + ValueShower[Right].GetComponent<Transform>().position.x) / 2, 0, 0);
+        transform.localScale = new Vector3((float)7.5*(Right - Left), 1, 1);
+    }
+}
