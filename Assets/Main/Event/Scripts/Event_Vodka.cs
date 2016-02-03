ba9811865f6e8306f0d99de5513993b60282b761
jayin301 <alswjdghk@snu.ac.kr>
2016-02-04 06:42:01 +0900

    connecting Event_Vodka.

--- Assets/Main/Event/Scripts/Event_Vodka.cs
+++ Assets/Main/Event/Scripts/Event_Vodka.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Event_Vodka : MonoBehaviour {
 
 	public GameObject EventMessage;
+    public int LeisureTime;
+    SelectBehavior Script;
 
 	void Start () {
 		Debug.Log ("Start Vodka");
@@ -11,13 +13,16 @@ public class Event_Vodka : MonoBehaviour {
 	}
 	
 
-	void Update () {
-	}
+	void Update ()
+    {
+        LeisureTime = Script.LeisureTime;
+    }
+	
 
 	void ActiveEvent()
 	{
 
-		if (Random.Range (1, 100) <= 100 && Event.canActiveEvent) {
+		if (Random.Range (1, 100) <= 100 && Event.canActiveEvent && LeisureTime >= 30) {
 			Event.canActiveEvent = false;
 			EventMessage.SetActive (true);
 		}
