f4090d6eb293a7540e993246cabf27daabc1a909
EQwon <bluetwintail@naver.com>
2016-02-12 11:34:21 +0900

    Create Dust After Regular Tiem

---
+++ Assets/Main/Script/CreateDust.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CreateDust : MonoBehaviour
+{
+    public GameObject Dust;
+    public int DeltaTime;
+
+    DateTime SysTime;
+    DateTime UpdatedTime;
+    TimeSpan Delta;
+
+    void Start()
+    {
+        Delta = new TimeSpan(0, 0, DeltaTime);
+        UpdatedTime = SysTime;
+    }
+
+    void Update()
+    {
+        SysTime = System.DateTime.Now;
+        if (TimeOver())
+        {
+            Instantiate(Dust);
+            UpdatedTime = SysTime;
+        }
+    }
+
+
+    bool TimeOver()
+    {
+        if (SysTime - UpdatedTime >= Delta)
+        {
+            return true;
+        }
+        else return false;
+    }
+}
