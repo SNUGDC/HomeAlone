450cf5950e242bb95d6c25187c1cb32f647d1c80
EQwon <bluetwintail@naver.com>
2016-02-16 23:40:54 +0900

    I want to Match Screen Size~

---
+++ Assets/ResolutionController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResolutionController : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(Screen.width);
+        Debug.Log(Screen.height);
+    }
+}
