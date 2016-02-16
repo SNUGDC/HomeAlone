9b7f752450a66391446345ef08bbcb1d03cd41e3
EQwon <bluetwintail@naver.com>
2016-02-17 02:48:04 +0900

    Create Big Dust

---
+++ Assets/Main/Script/CreateBigDust.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CreateBigDust : MonoBehaviour
+{
+    public GameObject BigDust;
+
+    TimeSpan TimeGap;
+    String TimeGapString;
+
+    void Start()
+    {
+        TimeGap = TimeCheck.OFFtime();
+        Debug.Log(TimeGap);
+
+        if(TimeGap > new TimeSpan(0,3,0))
+            Instantiate(BigDust);
+    }
+}
