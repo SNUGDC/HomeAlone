946dd56ff848c902f29e2b230dda15ae48aad92a
EQwon <bluetwintail@naver.com>
2016-02-16 23:03:00 +0900

    Make Quit By Escape Key

--- Assets/Main/Script/QuitApplication.cs
+++ Assets/Main/Script/QuitApplication.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class QuitApplication : MonoBehaviour
 {
+    public GameObject UI_Quit;
+
     void Update()
     {
         if (Input.GetKey("escape"))
+        {
+            UI_Quit.SetActive(true);
+        }
+    }
+
+    public void Quit()
+    {
             Application.Quit();
     }
 }
