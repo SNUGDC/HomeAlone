e5176d52e12a391ca236d03756e904398a0f5a66
ricegood <951227@gmail.com>
2016-02-17 10:41:12 +0900

    TalkList
    roughly

--- Assets/Main/Friends/Scripts/TalkBalloon.cs
+++ Assets/Main/Friends/Scripts/TalkBalloon.cs
@@ -3,14 +3,16 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class TalkBalloon : MonoBehaviour {
-	public string[] Text;
+	public string[] Text, TextSubject, SavedValue;
 	public int[] TextProbability;	// same size to Text & SUM is 100
 	public Text PanelText;
 	private int RandomNumber, saveNumber;
 
+	public bool[] IsAlreadyShow = {false};
+
 	// Use this for initialization
 	void Start () {
-		
+		load ();
 	}
 	
 	// Update is called once per frame
@@ -20,13 +22,31 @@ public class TalkBalloon : MonoBehaviour {
 
 	public void SendTextToPanel(){
 		saveNumber = RandomNumber;
-		if (saveNumber <= TextProbability [0])
-			PanelText.text = Text[0];
-
-		else for (int i = 1; i < TextProbability.Length; i++) {
-				if ( (TextProbability [i-1] < saveNumber) && (saveNumber <= TextProbability [i]))
-				PanelText.text = Text[i];
+		if (saveNumber <= TextProbability [0]) {
+			PanelText.text = Text [0];
+			IsAlreadyShow [0] = true;
+			save ();
 		}
 
+		else for (int i = 1; i < TextProbability.Length; i++) {
+				if ((TextProbability [i - 1] < saveNumber) && (saveNumber <= TextProbability [i])) {
+					PanelText.text = Text [i];
+					IsAlreadyShow [i] = true;
+					save ();
+				}
+		}
+
+	}
+
+	private void save(){
+		for (int i = 0; i < Text.Length; i++) {
+			PlayerPrefs.SetString(SavedValue[i],IsAlreadyShow[i].ToString());
+		}
+	}
+
+	public void load(){
+		for (int i = 0; i < Text.Length; i++) {
+			IsAlreadyShow [i] = (PlayerPrefs.GetString(SavedValue [i]) == "True");
+		}
 	}
 }
---
+++ Assets/Main/Friends/Scripts/TalkList.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TalkList : MonoBehaviour {
+	private bool[] IsSaved;
+	private string[] text;
+
+	public Button[] ButtonList;
+	public Text[] ButtonTextList;
+	public Text PanelText;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OnMouseDown(int i){
+		PanelText.text = text[i];
+	}
+
+	public void SetTalkList(GameObject TalkBalloon){
+		Debug.Log ("SetTalkList!");
+		TalkBalloon.GetComponent<TalkBalloon> ().load();
+		text = TalkBalloon.GetComponent<TalkBalloon> ().Text;
+		IsSaved = TalkBalloon.GetComponent<TalkBalloon> ().IsAlreadyShow;
+
+		for (int i = 0; i < ButtonTextList.Length; i++) {
+			ButtonTextList [i].text = "?";
+			ButtonList [i].enabled = false;
+			if (IsSaved [i]) {
+				ButtonTextList [i].text = TalkBalloon.GetComponent<TalkBalloon> ().TextSubject [i];
+				ButtonList [i].enabled = true;
+				Debug.Log ("Saved");
+			}
+		}
+	}
+}
