cdf3afd113750c6cb62a7d6d2a490b9941d3027b
ricegood <951227@gmail.com>
2016-02-17 12:07:11 +0900

    TImeCheck

--- Assets/Main/Script/TimeCheck.cs
+++ Assets/Main/Script/TimeCheck.cs
@@ -5,40 +5,39 @@ using System;
 
 public class TimeCheck : MonoBehaviour {
 	public static string LoadTime;
-	public static DateTime SysTime, LoadDateTime;
+	public static DateTime SysTime, UpdateTime, LoadDateTime;
 	public static TimeSpan Deltazero;
 
 	// Use this for initialization
 	void Start () {
 		SysTime = System.DateTime.Now;
+		UpdateTime = SysTime;
 		Deltazero = new TimeSpan (0, 0, 0);
+		if (PlayerPrefs.HasKey ("TimeSave")) {
+			LoadTime = PlayerPrefs.GetString ("TimeSave");
+			LoadDateTime = System.DateTime.Parse (LoadTime);
+		} else
+			LoadDateTime = SysTime;
+
+		Debug.Log(SysTime);
+		Debug.Log(LoadDateTime);
+		Debug.Log(SysTime - LoadDateTime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		PlayerPrefs.SetString("TimeSave", SysTime.ToString());
+		UpdateTime = System.DateTime.Now;
+		PlayerPrefs.SetString("TimeSave", UpdateTime.ToString());
 	}
 
 	public static bool TimeOver(TimeSpan Delta){
-		if (PlayerPrefs.HasKey ("TimeSave")) {
-			LoadTime = PlayerPrefs.GetString ("TimeSave");
-			LoadDateTime = System.DateTime.Parse (LoadTime);
-
-			if (SysTime - LoadDateTime > Delta)
-				return true;
-			else
-				return false;
-		} else
+		if (SysTime - LoadDateTime > Delta)
+			return true;
+		else
 			return false;
 	}
 
 	public static TimeSpan OFFtime(){
-		if (PlayerPrefs.HasKey ("TimeSave")) {
-			LoadTime = PlayerPrefs.GetString ("TimeSave");
-			LoadDateTime = System.DateTime.Parse (LoadTime);
-			return SysTime - LoadDateTime;
-		} else
-			return Deltazero;
+		return SysTime - LoadDateTime;
 	}
-
 }
