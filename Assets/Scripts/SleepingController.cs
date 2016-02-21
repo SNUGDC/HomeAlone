db4396a8fd0c3cc23d3026c96228a8dc51debe11
EQwon <bluetwintail@naver.com>
2016-02-21 20:19:15 +0900

    Making Sleeping Controller

---
+++ Assets/Scripts/SleepingController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SleepingController : MonoBehaviour
+{
+    public int SleepPercent;
+
+    void Start()
+    {
+        if(Random.Range(0,7) <= SleepPercent)
+        {
+            Debug.Log("Player is Sleeping");
+        }
+    }
+}
