e9b9cd24fe9378ecb0bde07c85af8d4ebb40bc5d
EQwon <bluetwintail@naver.com>
2016-02-25 04:20:29 +0900

    Add Image of Penguin To Event Scene

---
+++ Assets/Main/Event/ChangePenguinImage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePenguinImage : MonoBehaviour
+{
+    public Sprite PenguinDrunk;
+
+    void Update()
+    {
+        if(PenguinEventText.ClickedTime_PenguinEvent >3)
+        {
+            GetComponent<SpriteRenderer>().sprite = PenguinDrunk;
+        }
+    }
+}
