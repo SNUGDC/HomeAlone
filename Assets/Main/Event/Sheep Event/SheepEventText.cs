dd6ba9f11d1cc59098c49d2b70d58217bad1a2ff
EQwon <bluetwintail@naver.com>
2016-02-25 06:04:22 +0900

    Make Event Sheep

---
+++ Assets/Main/Event/Sheep Event/SheepEventText.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SheepEventText : MonoBehaviour
+{
+    public Text text;
+    public GameObject button;
+    public GameObject Sheep_Normal;
+    public GameObject Sheep_Cook;
+    public GameObject Sheep_CantCook;
+    public GameObject SheepForText;
+
+    public static int ClickedTime_SheepEvent;
+
+    void Start()
+    {
+        ClickedTime_SheepEvent = 0;
+    }
+
+    void Update()
+    {
+        switch (ClickedTime_SheepEvent)
+        {
+            case 0:
+                button.SetActive(true);
+                GetComponent<Text>().fontSize = 70;
+                text.text = "매번 얻어먹기만 하는 건 좀 눈치가 보여서 말야.";
+                break;
+            case 1:
+                text.text = "오늘은 내가! 떡볶이를 만들어 주지!";
+                break;
+            case 2:
+                text.text = "떡, 어묵, 파, 양념장만 있으면 간단하게 만들 수 있다구.";
+                break;
+            case 3:
+                text.text = "그럼 먼저 냄비에 물을 받아서...";
+                break;
+            case 4:
+                SpriteEnabled(Sheep_Normal, false);
+                SpriteEnabled(Sheep_Cook, true);
+                button.SetActive(false);
+                break;
+            case 5:
+                SpriteEnabled(Sheep_Cook, false);
+                SpriteEnabled(Sheep_CantCook, true);
+                button.SetActive(false);
+                break;
+            case 6:
+                text.text = "(냄비를 향해 손을 뻗으나 키가 작아서 손잡이가 닿질 않는듯 하다)";
+                break;
+            case 7:
+                button.SetActive(false);
+                break;
+            case 8:
+                SpriteEnabled(Sheep_Cook, true);
+                SpriteEnabled(Sheep_CantCook, false);
+                button.SetActive(false);
+                break;
+            case 9:
+                text.text = "냄ㅂ...";
+                break;
+            case 10:
+                button.SetActive(false);
+                break;
+            case 11:
+                SpriteEnabled(Sheep_Cook, false);
+                SpriteEnabled(Sheep_CantCook, true);
+                button.SetActive(false);
+                break;
+            case 12:
+                text.text = "냄비..냄비에...";
+                break;
+            case 13:
+                text.text = "물을..받아야...하는데...";
+                break;
+            case 14:
+                button.SetActive(false);
+                break;
+            case 15:
+                SpriteEnabled(Sheep_Cook, true);
+                SpriteEnabled(Sheep_CantCook, false);
+                button.SetActive(false);
+                break;
+            case 16:
+                SpriteEnabled(Sheep_Cook, false);
+                SpriteEnabled(Sheep_CantCook, true);
+                button.SetActive(false);
+                break;
+            case 17:
+                text.text = ".......";
+                break;
+            case 18:
+                button.SetActive(false);
+                break;
+            case 19:
+                SpriteEnabled(Sheep_Cook, true);
+                SpriteEnabled(Sheep_CantCook, false);
+                button.SetActive(false);
+                break;
+            case 20:
+                text.text = "마트 갔다올게.";
+                break;
+            case 21:
+                SpriteEnabled(Sheep_Cook, false);
+                button.SetActive(false);
+                break;
+            case 22:
+                text.text = "결국 인스턴트 떡볶이를 먹을 수 밖에 없었다...";
+                break;
+            default:
+                break;
+        }
+    }
+
+    void SpriteEnabled(GameObject Object, bool b)
+    {
+        Object.GetComponent<SpriteRenderer>().enabled = b;
+    }
+
+    public void IsClicked()
+    {
+        ClickedTime_SheepEvent = ClickedTime_SheepEvent + 1;
+    }
+}
---
+++ Assets/Main/Event/Sheep Event/TapRoom.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapRoom : MonoBehaviour
+{
+    public GameObject button;
+
+    void OnMouseUp()
+    {
+        switch(SheepEventText.ClickedTime_SheepEvent)
+        {
+            case 4:
+                SheepEventText.ClickedTime_SheepEvent = 5;
+                button.SetActive(true);
+                break;
+            case 5:
+                SheepEventText.ClickedTime_SheepEvent = 6;
+                button.SetActive(true);
+                break;
+            case 7:
+                SheepEventText.ClickedTime_SheepEvent = 8;
+                button.SetActive(true);
+                break;
+            case 8:
+                SheepEventText.ClickedTime_SheepEvent = 9;
+                button.SetActive(true);
+                break;
+            case 10:
+                SheepEventText.ClickedTime_SheepEvent = 11;
+                button.SetActive(true);
+                break;
+            case 11:
+                SheepEventText.ClickedTime_SheepEvent = 12;
+                button.SetActive(true);
+                break;
+            case 14:
+                SheepEventText.ClickedTime_SheepEvent = 15;
+                button.SetActive(true);
+                break;
+            case 15:
+                SheepEventText.ClickedTime_SheepEvent = 16;
+                button.SetActive(true);
+                break;
+            case 16:
+                SheepEventText.ClickedTime_SheepEvent = 17;
+                button.SetActive(true);
+                break;
+            case 18:
+                SheepEventText.ClickedTime_SheepEvent = 19;
+                button.SetActive(true);
+                break;
+            case 19:
+                SheepEventText.ClickedTime_SheepEvent = 20;
+                button.SetActive(true);
+                break;
+            case 21:
+                SheepEventText.ClickedTime_SheepEvent = 22;
+                button.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+}
