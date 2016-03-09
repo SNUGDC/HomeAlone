dd14043a58a2f23d625c6f67f5232dec7687bd28
EQwon <bluetwintail@naver.com>
2016-03-10 00:10:15 +0900

    Making Wastebin Event

---
+++ Assets/Main/Script/WasteBinController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WasteBinController : MonoBehaviour
+{
+    public int FullOfTrash;
+    public Sprite DirtyTrashCan;
+    public Sprite CleanTrashCan;
+
+    int Ran;
+    bool IsFull;
+    
+    void Start()
+    {
+        Ran = UnityEngine.Random.Range(0, 100);
+
+        if (Ran < FullOfTrash)
+        {
+            IsFull = true;
+            GetComponent<SpriteRenderer>().sprite = DirtyTrashCan;
+        }
+        else
+        {
+            IsFull = false;
+            GetComponent<SpriteRenderer>().sprite = CleanTrashCan;
+        }
+    }
+}
