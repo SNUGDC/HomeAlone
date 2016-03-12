d6d7084fa20fa243dd83432e94da6b58330865a7
ricegood <951227@gmail.com>
2016-03-12 14:52:34 +0900

    window

---
+++ Assets/window.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class window : MonoBehaviour {
+	public static bool open;
+	public GameObject openedImage, closedImage;
+	// Use this for initialization
+	void Start () {
+		load ();
+		openedImage.SetActive (open);
+		closedImage.SetActive (!open);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void save(){
+		PlayerPrefs.SetString ("window", open.ToString());
+	}
+
+	void load(){
+		open = (PlayerPrefs.GetString ("window") == "True");
+	}
+
+	public void toggle(){
+		//close
+		if (open) {
+			openedImage.SetActive (false);
+			closedImage.SetActive (true);
+			open = false;
+			save ();
+		}
+
+		//open
+		else {
+			openedImage.SetActive (true);
+			closedImage.SetActive (false);
+			open = true;
+			save ();
+		}
+	}
+}
