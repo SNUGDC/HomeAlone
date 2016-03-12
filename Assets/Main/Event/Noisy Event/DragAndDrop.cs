cfd1cb08f0e256aa4f1caeee42e0f88aed9e5b57
EQwon <bluetwintail@naver.com>
2016-03-13 01:13:15 +0900

    Finish Drag And Drop

---
+++ Assets/Main/Event/Noisy Event/DragAndDrop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public Camera Cam;
+    Vector3 MousePos = Input.mousePosition;
+
+    void OnMouseDrag()
+    {
+        Debug.Log(Input.mousePosition);
+        GetComponent<Transform>().position = Cam.ScreenToWorldPoint(new Vector3(MousePos.x, MousePos.y, -Cam.transform.position.z));
+    }
+}
