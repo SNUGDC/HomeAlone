c82dddbae82e8804499214e36e5af9b6b84f524a
EQwon <bluetwintail@naver.com>
2016-03-14 20:13:58 +0900

    왜 안될까?

--- Assets/Main/Event/Noisy Event/DragAndDrop.cs
+++ Assets/Main/Event/Noisy Event/DragAndDrop.cs
@@ -4,11 +4,27 @@ using System.Collections;
 public class DragAndDrop : MonoBehaviour
 {
     public Camera Cam;
-    Vector3 MousePos = Input.mousePosition;
+    Vector3 MousePos;
+
+    void Update()
+    {
+        MousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Cam.transform.position.z);
+    }
 
     void OnMouseDrag()
     {
-        Debug.Log(Input.mousePosition);
-        GetComponent<Transform>().position = Cam.ScreenToWorldPoint(new Vector3(MousePos.x, MousePos.y, -Cam.transform.position.z));
+        GetComponent<Transform>().position = Cam.ScreenToWorldPoint(MousePos);
+    }
+
+    void OnMouseUp()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D Coll)
+    {
+        Debug.Log("제발 반응좀 해줘ㅠㅠ");
+        if (Coll.name != null)
+            Debug.Log(Coll.name);
     }
 }
