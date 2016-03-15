6f0448e259aacc00207a5fe8bc344605564d4282
jayin301 <alswjdghk@snu.ac.kr>
2016-03-15 21:34:15 +0900

    TalkPanel Proto.

---
+++ Assets/MiniGame_A/TalkPanelManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TalkPanelManager : MonoBehaviour
+{
+    ArrayList Text;
+    public GameObject Panel;
+
+    void Start ()
+    {
+	}
+}
