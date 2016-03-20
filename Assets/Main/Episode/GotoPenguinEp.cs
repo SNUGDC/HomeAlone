b518f9785b92af453373df9e843a885a91ea717c
EQwon <bluetwintail@naver.com>
2016-03-20 14:02:59 +0900

    Temporarily Go to PenguinEp

---
+++ Assets/Main/Episode/GotoPenguinEp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GotoPenguinEp : MonoBehaviour
+{
+    public void Go()
+    {
+        SceneManager.LoadScene("PenguinEp");
+    }
+}
