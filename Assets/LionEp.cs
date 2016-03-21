54440500b47a55518249cd38d4540a65cbcedc91
ricegood <951227@gmail.com>
2016-03-21 23:58:45 +0900

    lion Ep

---
+++ Assets/LionEp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LionEp : MonoBehaviour {
+	public GameObject dialogwindow, dialog, choose;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OpenChoose(int line){
+		if (dialog.GetComponent<Dialog> ().LineNumber == line) {
+			choose.SetActive (true);
+			dialogwindow.SetActive (false);
+		}
+	}
+}
