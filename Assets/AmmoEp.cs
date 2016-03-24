497164630429f5258c0853436545d3bfb9a6abb1
ricegood <951227@gmail.com>
2016-03-24 21:22:54 +0900

    ammo sound

---
+++ Assets/AmmoEp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoEp : MonoBehaviour {
+	public GameObject sound, dialog;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Play(){
+		if (dialog.GetComponent<Dialog> ().LineNumber == 4)
+			sound.GetComponent<AudioSource> ().Play ();
+	}
+}
