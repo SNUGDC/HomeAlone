5591c9584d145ae56f38de8849f03da924980159
EQwon <bluetwintail@naver.com>
2016-03-26 22:14:44 +0900

    Finish After Real Ending

--- Assets/Main/Ending/BackToStartScene.cs
+++ Assets/Main/Ending/BackToStartScene.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class BackToStart : MonoBehaviour
+public class BackToStartScene : MonoBehaviour
 {
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.Escape))
             SceneManager.LoadScene("Start");
     }
 }
