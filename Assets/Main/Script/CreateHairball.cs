debbd25971b4510c42c187d3f159a33bd0fe3c55
EQwon <bluetwintail@naver.com>
2016-03-30 00:27:43 +0900

    Making Hairball

---
+++ Assets/Main/Script/CreateHairball.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CreateHairball : MonoBehaviour
+{
+    public GameObject Hairball1;
+    public GameObject Hairball2;
+    public GameObject Hairball3;
+    public GameObject Hairball4;
+    public GameObject Hairball5;
+    public int HairballRespawnTimeGap;
+    public int MaxHairball;
+
+    DateTime SysTime;
+    DateTime UpdatedTime;
+    TimeSpan Delta;
+    TimeSpan TimeGap;
+
+    int HairballAmount;
+    int k;
+
+    void Start()
+    {
+        SysTime = System.DateTime.Now;
+        Delta = new TimeSpan(0, 0, HairballRespawnTimeGap);
+        UpdatedTime = SysTime;
+
+        LoadHairball();
+
+        TimeGap = TimeCheck.OFFtime();
+
+        for (TimeSpan Gap = TimeGap; Gap >= Delta && HairballAmount <= MaxHairball; Gap -= Delta)
+        {
+            HairballAmount = HairballAmount + 1;
+        }
+
+        for (int i = 0; i <= HairballAmount; i++)
+        {
+            int a = i % 5;
+            switch (a)
+            {
+                case 0:
+                    Instantiate(Hairball1);
+                    break;
+                case 1:
+                    Instantiate(Hairball2);
+                    break;
+                case 2:
+                    Instantiate(Hairball3);
+                    break;
+                case 3:
+                    Instantiate(Hairball4);
+                    break;
+                case 4:
+                    Instantiate(Hairball5);
+                    break;
+                default:
+                    Instantiate(Hairball1);
+                    break;
+            }
+        }
+
+        SaveHairball();
+    }
+
+    void Update()
+    {
+        LoadHairball();
+
+        k = UnityEngine.Random.Range(1, 6);
+        SysTime = System.DateTime.Now;
+
+        if (TimeOver() && HairballAmount < MaxHairball)
+        {
+            UpdatedTime = SysTime;
+
+            switch (k)
+            {
+                case 1:
+                    Instantiate(Hairball1);
+                    HairballAmount = HairballAmount + 1;
+                    break;
+                case 2:
+                    Instantiate(Hairball2);
+                    HairballAmount = HairballAmount + 1;
+                    break;
+                case 3:
+                    Instantiate(Hairball3);
+                    HairballAmount = HairballAmount + 1;
+                    break;
+                case 4:
+                    Instantiate(Hairball4);
+                    HairballAmount = HairballAmount + 1;
+                    break;
+                case 5:
+                    Instantiate(Hairball5);
+                    HairballAmount = HairballAmount + 1;
+                    break;
+                default:
+                    Instantiate(Hairball5);
+                    HairballAmount = HairballAmount + 1;
+                    break;
+            }
+        }
+        SaveHairball();
+    }
+
+    bool TimeOver()
+    {
+        if (SysTime - UpdatedTime >= Delta)
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    void SaveHairball()
+    {
+        PlayerPrefs.SetInt("HairballAmount", HairballAmount);
+    }
+
+    void LoadHairball()
+    {
+        HairballAmount = PlayerPrefs.GetInt("HairballAmount");
+    }
+}
---
+++ Assets/Scripts/HairballController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HairballController : MonoBehaviour
+{
+    public GameObject ObjectEXP;
+    public AudioClip ClickHairball;
+
+    Renderer HairballRend;
+    Renderer EXPRend;
+    int HairballAmount;
+    public static int DefaultHairballCatch;
+
+    void Start()
+    {
+        DefaultHairballCatch = PlayerPrefs.GetInt("DefaultDustCatch");
+        HairballRend = GetComponent<Renderer>();
+        EXPRend = ObjectEXP.GetComponent<Renderer>();
+    }
+
+    void OnMouseEnter()
+    {
+        HairballRend.enabled = false;
+        EXPRend.enabled = true;
+        GetComponent<Collider2D>().enabled = false;
+        EXPEffect();
+        HairballAmount = PlayerPrefs.GetInt("HairballAmount");
+        HairballAmount = HairballAmount - 1;
+        MoneySystem.money += 100;
+        DefaultHairballCatch++;
+        PlayerPrefs.SetInt("HairballAmount", HairballAmount);
+        PlayerPrefs.SetInt("DefaultHairballCatch", DefaultHairballCatch);
+        GetComponent<AudioSource>().PlayOneShot(ClickHairball);
+        Destroy(gameObject, 1);
+    }
+
+    void EXPEffect()
+    {
+        ObjectEXP.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1);
+    }
+}
