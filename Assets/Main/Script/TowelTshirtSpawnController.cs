ce85c75591b1e0140cb3c8fb544f174884cc5e7a
EQwon <bluetwintail@naver.com>
2016-03-30 21:51:06 +0900

    Correct Gompang's Spawn Condition

---
+++ Assets/Main/Script/TowelTshirtSpawnController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowelTshirtSpawnController : MonoBehaviour
+{
+    public GameObject Sheep;
+    public GameObject Croco;
+
+    void Start ()
+    {
+        if (Sheep.GetComponent<Friend>().HowFriendly() <= 1 || Croco.GetComponent<Friend>().HowFriendly() <= 1)
+            Destroy(gameObject);
+    }
+}
