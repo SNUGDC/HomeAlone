9a96963694fe6aef1c6c7819ca208e1089d45974
ricegood <951227@gmail.com>
2016-04-12 22:23:44 +0900

    lion event

--- Assets/Main/Event/Lion Event/LionEvent.cs
+++ Assets/Main/Event/Lion Event/LionEvent.cs
@@ -4,22 +4,25 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LionEvent : MonoBehaviour {
-	public GameObject lion1, lion2, lion3, arrow;
+	public GameObject lion1, lion2, lion3, surprise, arrow;
 	public GameObject Dialog;
 
 	public void ImageChange(){
 		if (Dialog.GetComponent<Dialog> ().LineNumber == 1) {
 			lion1.GetComponent<Image> ().enabled = false;
 			lion2.GetComponent<Image> ().enabled = true;
-		} else if (Dialog.GetComponent<Dialog> ().LineNumber == 3) {
+			surprise.SetActive (true);
+		} else if (Dialog.GetComponent<Dialog> ().LineNumber == 2) {
 			lion2.GetComponent<Image> ().enabled = false;
 			lion3.GetComponent<Image> ().enabled = true;
-		} else if (Dialog.GetComponent<Dialog> ().LineNumber == 5) {
+		} else if (Dialog.GetComponent<Dialog> ().LineNumber == 4) {
 			lion3.GetComponent<Image> ().enabled = false;
 			lion2.GetComponent<Image> ().enabled = true;
-		} else if (Dialog.GetComponent<Dialog> ().LineNumber == 6) {
+		} else if (Dialog.GetComponent<Dialog> ().LineNumber == 5) {
 			lion2.GetComponent<Image> ().enabled = false;
 			lion3.GetComponent<Image> ().enabled = true;
+		} else if (Dialog.GetComponent<Dialog> ().LineNumber == 6) {
+			surprise.SetActive (false);
 		} else if (Dialog.GetComponent<Dialog> ().LineNumber == 8) {
 			arrow.SetActive (true);
 			lion3.GetComponent<Button> ().enabled = true;
