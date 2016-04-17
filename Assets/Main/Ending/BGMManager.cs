e6fb8e86f5f9fb85c35fb481e474ec9aac0fef9e
Lee, Kyuho <modulo641@gmail.com>
2016-04-18 06:44:55 +0900

    진엔딩 음악 수정

--- Assets/Main/Ending/BGMManager.cs
+++ Assets/Main/Ending/BGMManager.cs
@@ -5,6 +5,7 @@ public class BGMManager : MonoBehaviour {
 
     public AudioClip bgm;
     public int numberOfLoop;
+    public float introDelay;
     void Start()
     {
         StartCoroutine(PlayAudio (numberOfLoop));
@@ -12,9 +13,10 @@ public class BGMManager : MonoBehaviour {
     
     IEnumerator PlayAudio(int times)
     {
+        yield return new WaitForSeconds(introDelay);
         for(int i=0; i<times; i++)
         {
-            GetComponent<AudioSource>().PlayOneShot(bgm, 0.7F);
+            GetComponent<AudioSource>().PlayOneShot(bgm, introDelay);
             yield return new WaitForSeconds(bgm.length);
         }
     }
