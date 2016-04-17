0a842a250dac4d54158ab7552712fa9349c66b9a
pieridae <shadeofnihility@gmail.com>
2016-04-18 06:25:31 +0900

    Sheep Episode change
    character expression change

---
+++ Assets/SheepEp.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepEp : MonoBehaviour
+{
+    public GameObject DialogWindow, Dialog, player, playersad, sheep, sheepdoubt, sheepblank;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ImageChange()
+    {
+        if (Dialog.GetComponent<Dialog>().LineNumber == 1)
+        {
+            sheep.SetActive(false);
+            sheepdoubt.SetActive(true);
+        }
+        else if (Dialog.GetComponent<Dialog>().LineNumber == 6)
+        {
+            sheepblank.SetActive(true);
+            sheepdoubt.SetActive(false);
+            player.SetActive(false);
+            playersad.SetActive(true);
+        }
+    }
+}
+
+
