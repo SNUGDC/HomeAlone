5066afbc1c031c9135f4635e0718c4231b4cb40a
Lee, Kyuho <modulo641@gmail.com>
2016-04-18 06:11:56 +0900

    진엔딩 씬 음악 4번 재생

---
+++ Assets/Main/Ending/BGMManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMManager : MonoBehaviour {
+
+    public AudioClip bgm;
+    public int numberOfLoop;
+    void Start()
+    {
+        StartCoroutine(PlayAudio (numberOfLoop));
+    }
+    
+    IEnumerator PlayAudio(int times)
+    {
+        for(int i=0; i<times; i++)
+        {
+            GetComponent<AudioSource>().PlayOneShot(bgm, 0.7F);
+            yield return new WaitForSeconds(bgm.length);
+        }
+    }
+
+}
