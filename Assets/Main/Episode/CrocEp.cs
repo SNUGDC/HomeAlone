cec04ce557c51ab723da93319aa1cabd9fec1338
pieridae <shadeofnihility@gmail.com>
2016-04-18 05:56:45 +0900

    Croc episode change
    Character expression change

---
+++ Assets/Main/Episode/CrocEp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrocEp : MonoBehaviour
+{
+    public GameObject DialogWindow, Dialog, player, playersad, croc, crocsad;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ImageChange()
+    {
+        if (Dialog.GetComponent<Dialog>().LineNumber == 2)
+        {
+            croc.SetActive(false);
+            player.SetActive(false);
+            crocsad.SetActive(true);
+            playersad.SetActive(true);
+        }
+    }
+}
