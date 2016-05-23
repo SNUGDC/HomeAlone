2adde9fa63e95ee081fd4f1789aeeceb090a0913
EQwon <bluetwintail0906@gmail.com>
2016-05-23 23:36:28 +0900

    Make to Show Random Text

---
+++ Assets/Main/Script/ObjectInfoTextManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ObjectInfoTextManager : MonoBehaviour
+{
+    public Text InfoText;
+    public string[] Text;
+
+    int i;
+
+    public void InfoNumber()
+    {
+        i = Random.Range(0, 5);
+        InfoText.text = Text[i];
+    }
+}
