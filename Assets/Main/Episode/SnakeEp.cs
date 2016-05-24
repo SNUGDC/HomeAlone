f274ae568ed98ec3c5e3b4328edb005dd0a9673f
ricegood <951227@gmail.com>
2016-05-25 00:24:19 +0900

    SnakeEp

--- Assets/Main/Episode/SnakeEp.cs
+++ Assets/Main/Episode/SnakeEp.cs
@@ -8,7 +8,7 @@ public class SnakeEp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		IsAlreadyShow = (PlayerPrefs.GetString (IsAlreadyShowSaveName) == "False");
+		IsAlreadyShow = (PlayerPrefs.GetString (IsAlreadyShowSaveName) == "True");
 		PlayerPrefs.SetString (IsAlreadyShowSaveName, "True");
 		SaveAnswer = (PlayerPrefs.GetString (AnswerSaveName) == "True");
 		Debug.Log ("Save Answer : " + SaveAnswer);
