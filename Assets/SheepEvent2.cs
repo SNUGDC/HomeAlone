8d482565c6066693bcfdffdf086ce806dc5ad25a
ricegood <951227@gmail.com>
2016-05-25 16:47:04 +0900

    lion, sheep (미완)

---
+++ Assets/SheepEvent2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepEvent2 : MonoBehaviour {
+	public GameObject Dialog, Player, Sheep, Lion, Image1, Image2, Image3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ImageChange(){
+		if (Dialog.GetComponent<Dialog> ().LineNumber == 1) {
+			Player.SetActive (false);
+		}
+	}
+}
