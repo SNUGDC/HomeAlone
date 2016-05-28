4d8cb4f63d86c5ff69734fe4c072dcaddd2dc50c
EQwon <bluetwintail0906@gmail.com>
2016-05-28 18:10:14 +0900

    Finish BGM Controller

--- Assets/Main/Script/BGMController.cs
+++ Assets/Main/Script/BGMController.cs
@@ -1,19 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class BGMController : MonoBehaviour
 {
-    public Sprite BGMOn;
+    public Sprite BGMOff;
+    public GameObject Room;
+    public GameObject BGMButton;
+
+    string Temp_String;
+    Image ButtonImage;
 
     void Start()
     {
-        if (PlayerPrefs.GetString("BGM") == "ON")
+        ButtonImage = BGMButton.GetComponent<Image>();
+        if (PlayerPrefs.GetString("BGM") == "OFF")
         {
-            GetComponent<SpriteRenderer>().sprite = null;
+            ButtonImage.sprite = BGMOff;
+            ButtonImage.color = new Vector4(ButtonImage.color.r, ButtonImage.color.g, ButtonImage.color.b, 1);
         }
         else
         {
-            GetComponent<SpriteRenderer>().sprite = BGMOn;
+            ButtonImage.sprite = null;
+            ButtonImage.color = new Vector4(ButtonImage.color.r, ButtonImage.color.g, ButtonImage.color.b, 0);
         }
+    }
+
+    void Update()
+    {
+        if (Temp_String != PlayerPrefs.GetString("BGM"))
+        {
+            if (PlayerPrefs.GetString("BGM") == "OFF")
+            {
+                ButtonImage.sprite = BGMOff;
+                ButtonImage.color = new Vector4(ButtonImage.color.r, ButtonImage.color.g, ButtonImage.color.b, 1);
+                Room.GetComponent<AudioSource>().Stop();
+            }
+            else
+            {
+                ButtonImage.sprite = null;
+                ButtonImage.color = new Vector4(ButtonImage.color.r, ButtonImage.color.g, ButtonImage.color.b, 0);
+                Room.GetComponent<AudioSource>().Play();
+            }
+        }
+
+        Temp_String = PlayerPrefs.GetString("BGM");
+    }
+
+    public void ChangeBGMState()
+    {
+        if (PlayerPrefs.GetString("BGM") == "ON")
+            PlayerPrefs.SetString("BGM", "OFF");
+        else
+            PlayerPrefs.SetString("BGM", "ON");
     }
 }
