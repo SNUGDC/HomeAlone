84847886acd935eea0006500882917f1409da4d7
EQwon <bluetwintail0906@gmail.com>
2016-05-29 13:11:05 +0900

    Finish Modify Cam Event

---
+++ Assets/Main/Event/Cameleon Event/FadeOutCam_OnWall_Smile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeOutCam_OnWall_Smile : MonoBehaviour
+{
+    public Button DialogueButton;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = new Vector4(1, 1, 1, 1);
+        DialogueButton.interactable = false;
+    }
+
+    void Update()
+    {
+        DialogueButton.interactable = false;
+        GetComponent<SpriteRenderer>().color = new Vector4(GetComponent<SpriteRenderer>().color.r, GetComponent<SpriteRenderer>().color.g, GetComponent<SpriteRenderer>().color.b, GetComponent<SpriteRenderer>().color.a - 0.02f);
+        if (GetComponent<SpriteRenderer>().color.a < 0)
+        {
+            DialogueButton.interactable = true;
+        }
+    }
+}
